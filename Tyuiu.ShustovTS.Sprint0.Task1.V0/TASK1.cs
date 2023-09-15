adb547f8717f5ae9b84c73d0570b7c9d11881f3e
Шустов Тима <tima_shustov005@mail.ru>
2023-09-16 00:34:09 +0500

    task1

---
+++ Tyuiu.ShustovTS.Sprint0.Task1.V0/TASK1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Tyuiu.ShustovTS.Sprint0.Task1.V0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World");
+            string name = "Тимофей";
+            int age = 45;
+            Console.WriteLine("Здравствуйте меня зовут " + name + " мне " + age + " лет!");
+           Console.ReadKey();
+        }
+    }
+}
---
+++ Tyuiu.ShustovTS.Sprint0.Task2.V0.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.ShustovTS.Sprint0.Task2.V0.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.ShustovTS.Sprint0.Task2.V0.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("ee492564-d7f4-48a2-a563-6808534249b0")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
