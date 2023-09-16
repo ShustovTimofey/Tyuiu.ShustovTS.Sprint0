655e18d38c55abc28077011519e1f4ef49483eed
Шустов Тима <tima_shustov005@mail.ru>
2023-09-16 19:39:23 +0500

    Task2

--- Tyuiu.ShustovTS.Sprint0.Task2.V0/Program.cs
+++ Tyuiu.ShustovTS.Sprint0.Task2.V0/Program.cs
@@ -11,7 +11,7 @@ namespace Tyuiu.ShustovTS.Sprint0.Task2.V0
     {
         static void Main(string[] args)
         {
-            Console.WriteLine(DataService.GetMessage("Влад"));
+            Console.WriteLine(DataService.GetMessage("Tимофей"));
             Console.ReadKey();
         }
     }
